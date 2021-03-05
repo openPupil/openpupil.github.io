84651d39a99217d89a2bb654ec7952ae272c74e0
lodemo <2711650+lodemo@users.noreply.github.com>
2021-03-05 17:39:07 +0100

    fixed imports

--- _pages/datasets.md
+++ _pages/datasets.md
@@ -12,6 +12,7 @@ front-page-width: 10 # Describes the width of the page in columns from 1 to 12, 
 <div class="table_wrapper">
     <div id="table_filter">
     </div>
+    
     {% include table.html %}
 
 </div>
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -23,71 +23,71 @@ $( document ).ready(function() {
     });
 
 
-    // Datatables and chart
-    var table = $('#posts_table').DataTable({
-        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
-        responsive: {
-            details: {
-				type: 'column',
-                target: 'tr',
-			}
-        },
-        columnDefs: [
-            { responsivePriority: 1, targets: -1 },
-        ]
-    });
+    // // Datatables and chart
+    // var table = $('#posts_table').DataTable({
+    //     "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+    //     responsive: {
+    //         details: {
+	// 			type: 'column',
+    //             target: 'tr',
+	// 		}
+    //     },
+    //     columnDefs: [
+    //         { responsivePriority: 1, targets: -1 },
+    //     ]
+    // });
 
-    var chartContainer = $('#table_chart');
-    var searchContainer = $('#table_filter');
-
-
-    new $.fn.dataTable.SearchPanes(table, {});
-    table.searchPanes.container().prependTo(searchContainer);
-    table.searchPanes.rebuildPane();
+    // var chartContainer = $('#table_chart');
+    // var searchContainer = $('#table_filter');
 
 
-    var chart = Highcharts.chart(chartContainer[0], {
-        chart: {
-            type: 'pie',
-            backgroundColor: '#fff',
-        },
-        title: {
-            text: '',
-        },
-        series: [
-            {
-                data: chartData(table),
-            },
-        ],
-    });
+    // new $.fn.dataTable.SearchPanes(table, {});
+    // table.searchPanes.container().prependTo(searchContainer);
+    // table.searchPanes.rebuildPane();
+
+
+    // var chart = Highcharts.chart(chartContainer[0], {
+    //     chart: {
+    //         type: 'pie',
+    //         backgroundColor: '#fff',
+    //     },
+    //     title: {
+    //         text: '',
+    //     },
+    //     series: [
+    //         {
+    //             data: chartData(table),
+    //         },
+    //     ],
+    // });
  
-    // On each draw, update the data in the chart
-    table.on('draw', function () {
-        chart.series[0].setData(chartData(table));
-    });
+    // // On each draw, update the data in the chart
+    // table.on('draw', function () {
+    //     chart.series[0].setData(chartData(table));
+    // });
 
 });
 
-function chartData(table) {
-    var counts = {};
+// function chartData(table) {
+//     var counts = {};
  
-    // Count the number of entries for each position
-    table
-        .column(3, { search: 'applied' })
-        .data()
-        .each(function (val) {
-            if (counts[val]) {
-                counts[val] += 1;
-            } else {
-                counts[val] = 1;
-            }
-        });
+//     // Count the number of entries for each position
+//     table
+//         .column(3, { search: 'applied' })
+//         .data()
+//         .each(function (val) {
+//             if (counts[val]) {
+//                 counts[val] += 1;
+//             } else {
+//                 counts[val] = 1;
+//             }
+//         });
  
-    // And map it to the format highcharts uses
-    return $.map(counts, function (val, key) {
-        return {
-            name: key,
-            y: val,
-        };
-    });
-}
+//     // And map it to the format highcharts uses
+//     return $.map(counts, function (val, key) {
+//         return {
+//             name: key,
+//             y: val,
+//         };
+//     });
+// }
